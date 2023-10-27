2bdeb34b8755616520d7cc49a2351d499236649a
Bulgat <3104261648@qq.com>
2023-10-27 15:12:56 +0800

    commit test

---
+++ PMpro/src/main/java/cn/edu/xjtu/pmpro/PMproApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.xjtu.pmpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PMproApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PMproApplication.class, args);
+    }
+
+}
